2fe798e5e4e5a07bcb77a7c22055a87a61d15951
SinachPat <sinachpat@gmail.com>
2026-03-21 08:31:04 +0100

    updated deprecated files

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,15 +1,6 @@
-import posthog from 'posthog-js'
-import posthog from 'use client'
+import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: '2026-01-30'
-})
-
-export default function CheckoutPage() {
-    function handlePurchase() {
-        posthog.capture('purchase_completed', { amount: 99 })
-    }
-
-    return <button onClick={handlePurchase}>Complete purchase</button>
-}
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com",
+  defaults: "2026-01-30",
+});
